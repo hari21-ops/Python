7a8d575167b4f1dd3942d6b6a1b237f7b9c5600e
hari21-ops <compgitht@yahoo.com>
2025-04-10 22:16:51 +0530

    Update tests.py

--- blog_project/blog/articles/tests.py
+++ blog_project/blog/articles/tests.py
@@ -4,3 +4,7 @@ from django.test import TestCase
 class SanityTest(TestCase):
     def test_basic_math(self):
         self.assertEqual(2 + 2, 4)
+
+class MyTests(TestCase):
+    def test_example(self):
+        self.assertEqual(1 - 1, 2)
